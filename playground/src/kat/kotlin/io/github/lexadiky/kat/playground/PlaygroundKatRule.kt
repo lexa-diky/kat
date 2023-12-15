fd38e70dbbd8fc1da9b00fc897bc00aa94136591
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2023-12-15 13:08:39 +0700

    better usage example

--- playground/src/kat/kotlin/io/github/lexadiky/kat/playground/PlaygroundKatRule.kt
+++ playground/src/kat/kotlin/io/github/lexadiky/kat/playground/PlaygroundKatRule.kt
@@ -1,20 +1,27 @@
 package io.github.lexadiky.kat.playground
 
 import io.github.lexadiky.kat.sdk.KatRule
+import io.github.lexadiky.kat.sdk.dsl.nodes.file
 import io.github.lexadiky.kat.sdk.dsl.nodes.regularClass
 import io.github.lexadiky.kat.sdk.dsl.property.assertion.hasSupertype
 import io.github.lexadiky.kat.sdk.dsl.property.assertion.like
 
 class PlaygroundKatRule : KatRule({
-    regularClass {
-        any {
-            type hasSupertype "io.github.lexadiky.myapp.Renderer"
-            name like "Pika.*"
-        }
-
+    file {
+        name like "main.kt"
         validate {
-            any {
-                name like ".*Renderer"
+            children.regularClass {
+                any {
+                    type hasSupertype "io.github.lexadiky.myapp.Renderer"
+                    name like "Pika.*"
+                }
+
+                validate {
+
+                    any {
+                        name like ".*Renderer"
+                    }
+                }
             }
         }
     }
